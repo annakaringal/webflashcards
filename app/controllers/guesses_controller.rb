8b2294dec65ed075e2dd65e5fec5d4fb2dab93a1
aderend <d.dogrusoy@gmail.com>
2015-07-11 14:30:16 -0400

    implement submitting answers, moving on to next question and finishing round

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,10 +1,22 @@
-post '/guess' do
-  card = params[:guess[:card_id]]
-  guess = Guess.new(params[:guess])
+put '/guess/:guess_id' do
+  guess = Guess.find_by(id: params[:guess_id])
+  card = Card.find(guess.card_id)
+  round = Round.find(guess.round_id)
   if guess.user_answer == card.answer
     guess.correct = true
   end
+  guess.attempts += 1
   guess.save!
+  next_guess = round.guesses.find_by(attempts: 0)
+
+  if next_guess.nil?
+    redirect "/rounds/#{round.id}/summary"
+  else
+    redirect "/rounds/#{round.id}/#{next_guess.card_id}"
+  end
+
 end
+
+
 
 
