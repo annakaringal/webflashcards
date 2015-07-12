d58cccf7d544f6cf118bdba17b8066b36d35c06c
Anna Karingal <anna.karingal@gmail.com>
2015-07-12 18:58:53 -0400

    Seed with Bees quiz

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 
-# DECKS = ["Food", "Sports", "Cars"]
-
 FOOD = {
 "The rice dish ‘paella’ comes from what country?" => "Spain",
 "Deer meat is known by what name?" => "Venison",
@@ -12,16 +10,23 @@ FOOD = {
 "True or false? Cooking food often transforms its chemical make up." => "True",
 }
 
-
-# DECKS.each do |deck|
-#   d = Deck.create(name: deck)
-#   d.cards << Card.new(question: "Question #{i}", answer: "Answer #{i}")
-# end
-
+BEES = {
+  "How many eyes does a bee have?" => "5",
+  "True or False, Bees can calculate the angles of the Earth." => "True",
+  "How many miles per hour can a honey bee fly?" => "5",
+  "How many pairs of wings do bees have?" => "2",
+  "What is the scientific name for honey bees?" => "Apis Mellifera",
+  "How far do bees travel from their hives?" => "3 miles",
+ }
 
 food = Deck.create(name: "Food")
 FOOD.each do |question, answer|
   food.cards << Card.new(question: question, answer: answer)
+end
+
+bees = Deck.create(name: "Bees")
+BEES.each do |question, answer|
+  bees.cards << Card.new(question: question, answer: answer)
 end
 
 USERS = ["Deren", "Jason", "Anna"]
@@ -29,8 +34,3 @@ USERS = ["Deren", "Jason", "Anna"]
 USERS.each do |user|
   User.create(user_name: user, password: "1234")
 end
-
-# Round.create(user_id: 1, deck_id: 1)
-
-# Guess.create(round_id: 1, card_id: 1, user_answer: "Bananas")
-
